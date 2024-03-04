ad89ed2a475daeae35c1446036fc51f6d7a68cb2
Neves-P <p.m.santos.neves@rug.nl>
2024-03-04 16:37:50 +0100

    Formatting and typos

--- bot/check-build.sh
+++ bot/check-build.sh
@@ -358,8 +358,8 @@ success_msg="no message matching <code>${GP_req_missing}</code>"
 failure_msg="found message matching <code>${GP_req_missing}</code>"
 CoDeList=${CoDeList}$(add_detail ${MISSING} 0 "${success_msg}" "${failure_msg}")
 
-success_msg="found message(s) saying built application ($n_built) match build requests ($n_to_build)"
-failure_msg="built applications ($n_built) don't match build requests ($n_to_build)"
+success_msg="found message(s) saying number of built applications (<code>$n_built</code>) matches the numebr of  build requests <code>($n_to_build)</code>"
+failure_msg="number of built applications (<code>$n_built</code>) doesn't match the number of build requests (<code>$n_to_build</code>)"
 CoDeList=${CoDeList}$(add_detail ${NO_MISSING} 1 "${success_msg}" "${failure_msg}")
 
 success_msg="found message matching <code>${GP_tgz_created}</code>"
